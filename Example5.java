b47e640d71f254c416967722ff8e5739afc704b4
MrYoungCodernator <119313098+Mr-YounG-Codernator@users.noreply.github.com>
2024-01-28 21:11:36 +0200

    Internet exercises
    Internet exercises

---
+++ Example5.java
@@ -0,0 +1,14 @@
+public class Example5 {
+
+    public static void main(String[] args) {
+
+     char s;
+
+     for(s = 'A'; s <= 'Z'; s++){
+
+         System.out.println(s);
+
+     }
+
+    }
+}
---
+++ Example5Point5.java
@@ -0,0 +1,15 @@
+public class Example5Point5 {
+
+    public static void main(String[] args) {
+
+     char s = 'A';
+
+     while(s <= 'Z'){
+
+         System.out.println(s);
+         s++;
+
+     }
+
+    }
+}

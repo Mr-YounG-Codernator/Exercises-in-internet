7704ccecd836b837dac9ad266961a2fd0e17e907
MrYoungCodernator <119313098+Mr-YounG-Codernator@users.noreply.github.com>
2024-01-29 18:01:11 +0200

    Internet exercises
    Internet exercises

---
+++ Example6.java
@@ -0,0 +1,14 @@
+public class Example6 {
+
+    public static void main(String[] args) {
+
+        char s;
+
+        for(s = 'Z'; s >= 'A'; s--  ){
+
+            System.out.println(s);
+
+        }
+
+    }
+}
---
+++ Example6Point6.java
@@ -0,0 +1,15 @@
+public class Example6Point6 {
+
+    public static void main(String[] args) {
+
+        char s = 'z';
+
+        while(s >= 'a'){
+
+            System.out.println(s);
+            s--;
+
+        }
+
+    }
+}
